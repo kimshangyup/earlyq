5efe606b0ee459a0a6ebe04f5e4f3f046c0a525b
kimshangyup <kmn1120@naver.com>
2017-01-21 16:31:09 +0900

    구글애널리틱스

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
+    url(r'^$', 'blog.views.test'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
 ]
